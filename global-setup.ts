54ba72a6a1cb7b131aa3ce93087de65247aef09a
HebePython <50890910+HebePython@users.noreply.github.com>
2025-04-01 15:03:18 +0200

    added folder to resource sfor storage-state json file.

--- global-setup.ts
+++ global-setup.ts
@@ -1,11 +1,24 @@
 import { chromium } from '@playwright/test';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function globalSetup() {
+  // Create the directory if it doesn't exist
+  const storeStatePath = path.join('resources', 'storage-state');
+  
+  if (!fs.existsSync(storeStatePath)) {
+    fs.mkdirSync(storeStatePath, { recursive: true });
+    console.log(`Created directory: ${storeStatePath}`);
+  }
+  
+  const storagePath = path.join(storeStatePath, 'storage-state.json');
+  
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-  
-  // Go to your site
+
+
+  // Go to site
   await page.goto('https://volvogroup.com');
   
   // Handle cookie consent
@@ -19,7 +32,7 @@ async function globalSetup() {
   }
   
   // Save storage state to a file
-  await context.storageState({ path: './storage-state.json' });
+  await context.storageState({ path: storagePath });
   
   // Close everything
   await browser.close();
--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     // Use the storage state in all tests
-    storageState: './storage-state.json',
+    storageState: './resources/storage-state/storage-state.json',
   },
 
   /* Configure projects for major browsers */
