3c82c1c0df75504be54f8d93807bcabf7d3f6778
HebePython <henrik.bergman.edu@gmail.com>
2025-05-20 12:05:05 +0200

    updated orange test for .env

--- tests/orangeHRM_tests/login_tests.spec.ts
+++ tests/orangeHRM_tests/login_tests.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '../../resources/util/base/orange_base'
+import 'dotenv/config';
 
-const { EMAIL, PW } = process.env
+const { EMAIL, PW } = process.env;
+
+if (!EMAIL || !PW) {
+        throw new Error("EMAIL and PW must be defined in the environment variables.");
+    }
 
 test("Log into orange HRM", async ({
     loginPage,
     dashboardPage
+
 }) => {
     await loginPage.goto();
-    await loginPage.login("Admin", "admin123"); // put these in .env later.
-
+    await loginPage.login(EMAIL, PW);
     await dashboardPage.isReady();
 });
